7ba4d9bfbd6cb9c33453ae512b01000022bdeb1e
ManoloMancelli <manollo.mancelli@gmail.com>
2026-06-25 22:22:32 +0200

    Update Config.h

--- Software/Firmware - For the boards/ESP-WROOM-32 Index - WiP/Right_Controller/Config.h
+++ Software/Firmware - For the boards/ESP-WROOM-32 Index - WiP/Right_Controller/Config.h
@@ -10,7 +10,7 @@
 #define FingerMiddlePin     99
 #define FingerRingPin       99
 #define FingerPinkyPin      99
-#define JoyXPin             99
+#define JoyXPin             35
 #define JoyYPin             21
 #define JoyClickPin         34
 #define VbatPin             99
